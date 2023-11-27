58b217c99663d7d0094bf23fd370ed42b70eb6d4
Salvo Catanese <salvocatanese@gmail.com>
2023-11-27 12:00:54 +0100

    Update Somma_N_Interi.cpp

--- Esercizio05/Somma_N_Interi.cpp
+++ Esercizio05/Somma_N_Interi.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  
+  # definizione delle variabili intere
+  int somma=0, i=0, n, numero;
+
+  # input numeri da sommare
+  cout << "Quanti numeri da sommare? ";
+  cin >> n;
+  
+  # iterazione
+  while (i < n) {
+      cout << "numero " << i+1 << ": ";
+      cin >> num
+      
+      # aggiunge  il valore inserito alla variabile che conterrà la somma degli n numeri interi 
+      somma = somma + num;  // si può scrivere con l'operatore di assegnamento composto +=
+                            // somma += num;
+      
+      # incrementa variabile contatore i
+      i = i + 1;            // si può scrivere utilizzando  l'operatore di incremento i++ 
+                            // equivalente all'operatore di assegnamento composto i += 1;
+    }
+  
+  # output
+  cout << "La somma degli " << n << " numeri e' " << somma;
+
+  return 0;
+}
